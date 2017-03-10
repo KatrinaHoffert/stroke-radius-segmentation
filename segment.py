f087b661c3e74f6885c9d2c34c579c536ec5e3cd
Kat Hoffert <kat.hoffert@gmail.com>
2017-03-10 17:56:05 -0600

    OneCut segmentation now runs

--- segment.py
+++ segment.py
@@ -6,7 +6,12 @@ binary segmentation images.
 import os, re, sys, subprocess
 from common import Study
 
-def segment(study, originals_loc, strokes_loc, output_loc, segmentation_program):
+# The Boykov segmentation program is only currently available as a Linux binary,
+# but the OneCut segmentation program is only currently available as a Windows
+# binary, so we have to flip by platform what we run...
+platform = "windows"
+
+def segment(study, originals_loc, strokes_loc, output_loc, run_segmentation_code):
     '''
     Segments images using the segmenation program.
 
@@ -14,7 +19,9 @@ def segment(study, originals_loc, strokes_loc, output_loc, segmentation_program)
     originals_loc: Folder that the original files are in.
     strokes_loc: Folder that the stroke/point annoted files are in.
     output_loc: Folder that the output files should be placed in.
-    segmentation_program: The segmentation application that is run on each image.
+    run_segmentation_code: Lambda that runs the actual segmentation program. Is
+    given the file ID, the path to the original image, the path to the strokes file,
+    an output folder, and a calculated output file name.
     '''
     i = 0
     files = os.listdir(strokes_loc)
@@ -51,21 +58,42 @@ def segment(study, originals_loc, strokes_loc, output_loc, segmentation_program)
         # Skip files that have already been segmented
         if os.path.exists(output_image): continue
 
-        # Note: Python 3.5+ this method is called "run"
-        subprocess.call([segmentation_program, '--strokefglabel=29', '--strokebglabel=149',
-            '--outputdir=' + output_loc, original_image, stroke_image])
+        run_segmentation_code(file_id, original_image, stroke_image, output_loc, output_image)
 
-        # Now move the created file to the right name and remove the contour file
-        created_file_base = output_loc + '/' + file_id + '.jpg'
-        os.rename(created_file_base + '.segmentation.tif', output_image)
-        os.remove(created_file_base + '.contours.tif')
+def run_boykov_segmentation(file_id, original_image, stroke_image, output_loc, output_image):
+    # Note: Python 3.5+ this method is called "run"
+    subprocess.call(['./segmentation_programs/BoykovMaxFlowGeneric', '--strokefglabel=29',
+        '--strokebglabel=149', '--outputdir=' + output_loc, original_image, stroke_image])
 
-print('Processing Rau\'s strokes')
-segment(Study.Rau, './rau/originals', './rau/dilated_strokes', './rau/segmented_strokes', './segmentation_programs/BoykovMaxFlowGeneric')
+    # Now move the created file to the right name and remove the contour file
+    created_file_base = output_loc + '/' + file_id + '.jpg'
+    os.rename(created_file_base + '.segmentation.tif', output_image)
+    os.remove(created_file_base + '.contours.tif')
 
-print('\nProcessing Rau\'s points')
-segment(Study.Rau, './rau/originals', './rau/dilated_points', './rau/segmented_points', './segmentation_programs/BoykovMaxFlowGeneric')
+def run_onecut_segmentation(file_id, original_image, stroke_image, output_loc, output_image):
+    # Note: Python 3.5+ this method is called "run"
+    subprocess.call(['./segmentation_programs/OneCut', original_image, stroke_image,
+        '--fg-label', '29', '--bg-label', '149', '--output', output_image], stdout=subprocess.DEVNULL)
 
-print('\nProcessing Yuanxia\'s strokes')
-segment(Study.Yuanxia, './yuanxia/originals', './yuanxia/dilated', './yuanxia/segmented', '../segmentation_programs/BoykovMaxFlowGeneric')
-print()
+if platform == "linux":
+    print('Segmenting with Boykov\'s graph cut:')
+    print('Processing Rau\'s strokes')
+    segment(Study.Rau, './rau/originals', './rau/dilated_strokes', './rau/segmented_strokes', run_boykov_segmentation)
+
+    print('\nProcessing Rau\'s points')
+    segment(Study.Rau, './rau/originals', './rau/dilated_points', './rau/segmented_points', run_boykov_segmentation)
+
+    print('\nProcessing Yuanxia\'s strokes')
+    segment(Study.Yuanxia, './yuanxia/originals', './yuanxia/dilated', './yuanxia/segmented', run_boykov_segmentation)
+    print()
+else:
+    print('Segmenting with OneCut:')
+    print('Processing Rau\'s strokes')
+    segment(Study.Rau, './rau/originals', './rau/dilated_strokes', './rau/segmented_strokes_onecut', run_onecut_segmentation)
+
+    print('\nProcessing Rau\'s points')
+    segment(Study.Rau, './rau/originals', './rau/dilated_points', './rau/segmented_points_onecut', run_onecut_segmentation)
+
+    print('\nProcessing Yuanxia\'s strokes')
+    segment(Study.Yuanxia, './yuanxia/originals', './yuanxia/dilated', './yuanxia/segmented_onecut', run_onecut_segmentation)
+    print()
